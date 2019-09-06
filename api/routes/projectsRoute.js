3559f5bf3b70f1325d4cbfffba50ee0e99f8292b
Arvin Agas <arvagas@gmail.com>
2019-09-06 12:32:30 -0400

    GET requests for projects

--- api/routes/projectsRoute.js
+++ api/routes/projectsRoute.js
@@ -6,6 +6,8 @@ const router = express.Router()
 
 // @@@@@@@@@@ Custom Middleware @@@@@@@@@@
 function validateProjectId (req, res, next) {
+    const { id } = req.params
+
     projDB.get(id)
     .then(userId => {
         if (userId) next()
@@ -13,5 +15,31 @@ function validateProjectId (req, res, next) {
     })
     .catch(err => res.status(500).json({ message: "error validating project id" }))
 }
+
+// @@@@@@@@@@ GET request @@@@@@@@@@
+//Get all projects
+router.get('/', (req,res) => {
+    projDB.get()
+    .then(projects => res.json(projects))
+    .catch(err => res.status(500).json({ message: "error retrieving projects" }))
+})
+
+//Get specific projects w/ actions
+router.get('/:id', validateProjectId, (req,res) => {
+    const { id } = req.params
+
+    projDB.get(id)
+    .then(project => res.json(project))
+    .catch(err => res.status(500).json({ message: "error retrieving specific project" }))
+})
+
+//Get only the actions of a specific project
+router.get('/:id/actions', validateProjectId, (req,res) => {
+    const { id } = req.params
+
+    projDB.getProjectActions(id)
+    .then(projectActions => res.json(projectActions))
+    .catch(err => res.status(500).json({ message: "error retrieving specific project" }))
+})
 
 module.exports = router
