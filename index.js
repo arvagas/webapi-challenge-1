09d3981032931ae58435bbee9173d25d9687b9c7
Arvin Agas <arvagas@gmail.com>
2019-09-06 15:04:01 -0400

    testing deployment to heroku with react and node in one folder

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 require('dotenv').config()
 
+const express = require('express')
+const path = require('path')
+
 const defaults = require('./config/defaults')
 const server = require('./api/server')
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    server.use(express.static(path.join(__dirname, 'client/build')));
+    
+    // Handle React routing, return all requests to React app
+    server.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 const port = defaults.port
 server.listen(port, () => {
