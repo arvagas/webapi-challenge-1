9ea4ba04457e5e182f6be9b7991c49ad7a7f10c2
Arvin Agas <arvagas@gmail.com>
2019-09-06 13:57:58 -0400

    heroku ready

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
+
+const defaults = require('./config/defaults')
 const server = require('./api/server')
 
-const port = 5000
+const port = defaults.port
 server.listen(port, () => {
     console.log(`*** Running on http://localhost:${port} ***`)
 })
